0fbe677082d1919527c3097167ac3f8873c7bdb7
jovan <jovancraft@gmail.com>
2024-01-25 04:25:41 -0600

    finished configuring the middleware functions/complete

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,24 @@
+const db = require('../../data/db-config')
+
 module.exports = {
   checkId,
   checkPayload,
 }
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+  const shipper = await db('shippers').where('shipperid', req.params.id).first()
+  if(!shipper){
+    next({ status: 404, message: `That ID does't exist!`})
+  } else {
+    next()
+  }
 }
 
 function checkPayload(req, res, next) {
-  next()
+  if(!req.body.Phone || !req.body.ShipperName){
+    //remember, SQL doesn't really care about capitalization but JAVASCRIPT DOES!!!
+    next({ status: 422, message: 'Phone and ShipperName required'})
+  } else {
+    next()
+  }
 }
