e7e632ec40fbb8e3b145753aac05b24058f7eadc
jovan <jovancraft@gmail.com>
2024-01-25 03:45:36 -0600

    finsihed the create function inside shippers-model.js and added a status code inside router.post inside shippers-routers.js

--- api/shippers/shippers-model.js
+++ api/shippers/shippers-model.js
@@ -20,16 +20,24 @@ async function get() {
 async function getById(shipperId) {
   //This, AGAIN, works but is not the right was to do this
   //const result = await db.raw('select * from shippers where shipperId = 1;')
-  const result = await db('shippers').where('shipperid', shipperId)
+  const [shipper] = await db('shippers').where('shipperid', shipperId)
   //.where({ }) could take in an object with a bunch of key-value pairs but since we only have shipperid, this is fine
+  //also wrapping shipper inside brackets makes it where when you log it out using httpie and an id doesn't exist its blank instead of an empty array apparently...
+  //could also be done ike this:
+  //const result = await db('shippers).where('shipperid', shipperId).first()
+  return shipper
+}
+
+async function create(shipper) {
+  const [shipperId] = await db('shippers').insert(shipper)
+  //after filling out the neccessar info in httpie, we get back an array with an integer(the number 4 in my case) inside! [ 4 ]
+  const result = await getById(shipperId)
+  //after changing it to use getById in the function, now instead of outputing the shipperId, it now shows the nexly created shipper object!!
   return result
 }
 
-async function create() {
-  return 'create wired'
-}
-
-async function update() {
+async function update(shipperId, shipper) {
+  
   return 'update wired'
 }
 
