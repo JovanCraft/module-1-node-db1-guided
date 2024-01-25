e28820976da4fe2009886df053d84a7558763a44
jovan <jovancraft@gmail.com>
2024-01-25 03:26:08 -0600

    finished the get and getById inside of shippers-model.js

--- api/shippers/shippers-model.js
+++ api/shippers/shippers-model.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config')
+
 module.exports = {
   get,
   getById,
@@ -7,11 +9,20 @@ module.exports = {
 }
 
 async function get() {
-  return 'get wired'
+  //This WORKS!! But is not the proper way...
+  //const result = await db.raw('select * from shippers;')
+  const result = await db('shippers')
+  //if you're not interested in all the columns you could do this behind it for example:
+  //.select('phone', 'shippername')
+  return result
 }
 
-async function getById() {
-  return 'getById wired'
+async function getById(shipperId) {
+  //This, AGAIN, works but is not the right was to do this
+  //const result = await db.raw('select * from shippers where shipperId = 1;')
+  const result = await db('shippers').where('shipperid', shipperId)
+  //.where({ }) could take in an object with a bunch of key-value pairs but since we only have shipperid, this is fine
+  return result
 }
 
 async function create() {
